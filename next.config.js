f0f79bd499706c8afc1be8f5904c16996c4e0cf5
TuHanren <tu.hanren@gmail.com>
2022-06-02 16:26:06 -0400

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+module.exports = withPlugins([optimizedImages], { target: "serverless" });
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
